5c2dd08b88c94ac69804633b1aadbc397a3fcf49
AndrewPonyk <andrew9999@ukr.net>
2016-01-27 02:04:36 +0200

    AutoriaAnalyzer in progress

--- auto-price-analysis/src/main/java/com/dto/Advert.java
+++ auto-price-analysis/src/main/java/com/dto/Advert.java
@@ -1,5 +1,8 @@
 package com.dto;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by andrii on 25.01.16.
  */
@@ -9,8 +12,9 @@ public class Advert {
     public Integer year;
     public String description;
 
-    public String price;
+    public Integer price;
     public Integer mileage;
+    private String hash;
 
     public Advert setName(String name) {
         this.name = name;
@@ -32,9 +36,28 @@ public class Advert {
         return this;
     }
 
-    public Advert setPrice(String price) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Advert advert = (Advert) o;
+        return this.getHash().equals(advert.getHash());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (year != null ? year.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    public Advert setPrice(Integer price) {
         this.price = price;
         return this;
+
     }
 
     public Advert setMileage(Integer mileage) {
@@ -44,6 +67,30 @@ public class Advert {
 
     @Override
     public String toString() {
-        return name + ":" + city + ":" + price;
+        return String.format("{%s}[%s][%s][%s][%s][%s][%s]", getHash(), name, city, price, year, mileage, description);
+    }
+
+    public String getHash(){
+        if(this.hash != null){
+            return this.hash;
+        }
+
+        String descriptionForHash = this.description.length() > 15 ? this.description.substring(0, 15) : this.description;
+        String original = this.name + this.city + this.year + descriptionForHash;
+        StringBuffer sb = new StringBuffer();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(original.getBytes());
+            byte[] digest = md.digest();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        this.hash = sb.toString().substring(0,5); // use first symbols as id of advert
+        return hash;
     }
 }
--- auto-price-analysis/src/main/java/com/general/SeleniumServiceImpl.java
+++ auto-price-analysis/src/main/java/com/general/SeleniumServiceImpl.java
@@ -1,16 +1,15 @@
 package com.general;
 
 import com.dto.Advert;
-import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,17 +38,50 @@ public class SeleniumServiceImpl implements SeleniumService {
         System.out.println(driver.findElement(carsCountSelector).getText());
 
         List<WebElement> carsAdverts = driver.findElements(By.cssSelector(".ticket-item"));
-        carsAdverts.forEach(advert -> {
-            allCars.add(new Advert().
-                            setName(advert.findElements(By.cssSelector(".item.ticket-title")).get(0).getText()).
-                            setCity(advert.findElements(By.cssSelector(".location a")).get(0).getText()).
-                            setPrice(advert.findElements(By.cssSelector(".price-ticket .size16")).get(0).getText())
-            );
+            carsAdverts.forEach(advert -> {
 
-        }
+                try{
+                    if(advert.findElements(By.cssSelector(".item.ticket-title")).size()>0){
+                        allCars.add(new Advert().
+                                        setName(advert.findElements(By.cssSelector(".item.ticket-title")).get(0).getText()).
+                                        setCity(advert.findElements(By.cssSelector(".location a")).get(0).getText()).
+                                        setYear(extractYear(advert.findElements(By.cssSelector(".item.ticket-title")).get(0).getText())).
+                                        setPrice(extractInteger(advert.findElements(By.cssSelector(".price-ticket .size16 strong")).get(0).getText())).
+                                        setMileage(extractInteger(advert.findElements(By.cssSelector(".characteristic .item-char")).get(0).getText())).
+                                        setDescription(advert.findElements(By.cssSelector(".descriptions-ticket")).get(0).getText())
+                        );
+                        System.out.println(allCars.get(allCars.size()-1));
+                    }
+                }catch (Exception e) {
+
+                }
+
+            }
         );
         //closeBrowser();
 
         return allCars;
+    }
+
+    public Integer extractYear (String name){
+        String result = name.replaceAll("[^0-9]", "");
+        if(result.length() > 4){
+            result = result.substring(result.length() - 4);
+        }
+        try {
+            return  Integer.parseInt(result);
+        }catch (Exception e){
+
+        }
+        return 0;
+    }
+
+    public Integer extractInteger(String price){
+        try{
+            return  Integer.parseInt(price.replaceAll("[^0-9]", ""));
+        }catch (Exception e){
+
+        }
+        return 0;
     }
 }
