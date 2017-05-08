d888eeeb94dea19579c13c96e231acbb5343e4e7
AndrewPonyk <andrew9999@ukr.net>
2017-05-08 09:41:14 +0300

    Some changes to selenium in WebUtils, logic memory - changes

--- WebUtilsWIthSelenium/WebUtilsUsingSelenium/src/main/java/javacodegeeks/JavaCodegeeksBrowser.java
+++ WebUtilsWIthSelenium/WebUtilsUsingSelenium/src/main/java/javacodegeeks/JavaCodegeeksBrowser.java
@@ -1,11 +1,15 @@
 package javacodegeeks;
 
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
 
 /**
  * Created by andrii on 06.03.17.
@@ -13,14 +17,22 @@ import java.util.List;
 public class JavaCodegeeksBrowser {
     static {
         System.setProperty("webdriver.gecko.driver", "/home/andrii/Programs/geckodriver");
+
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log","org.apache.commons.logging.impl.NoOpLog");
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
     }
-    static WebDriver driver =   new FirefoxDriver();
+
+    //static WebDriver driver =   new FirefoxDriver(); // works only for selenium 3.0.0 !!!!
+    static WebDriver driver =   new HtmlUnitDriver();
     static String url = "https://www.javacodegeeks.com/page/";
+
 
     public static void readArticleTitles(){
         WebElement closePopup;
+        final AtomicInteger counter = new AtomicInteger(1);
 
-        for(int i = 840;i>0;i--){
+        for(int i = 947;i>0;i--){
             System.out.println("------Page: " + i);
             driver.get(url+ i);
             try{
@@ -28,14 +40,16 @@ public class JavaCodegeeksBrowser {
                 if(closePopup != null){
                     closePopup.click();
                 }
-
             }catch (Exception e){
 
             }
 
 
             List<WebElement> posts = driver.findElements(By.className("post-title"));
-            posts.forEach(e->{System.out.println(e.getText());});
+            posts.forEach(e -> {
+                System.out.println(new StringBuilder("").append(counter.incrementAndGet())
+                        .append(") ").append(e.getText()));
+            });
         }
     }
 }
---
+++ WebUtilsWIthSelenium/WebUtilsUsingSelenium/src/main/java/javacodegeeks/JavaCodegeeksRetrieverApp.java
@@ -0,0 +1,10 @@
+package javacodegeeks;
+
+/**
+ * Created by andrii on 06.03.17.
+ */
+public class JavaCodegeeksRetrieverApp {
+    public static void main(String[] args) {
+        JavaCodegeeksBrowser.readArticleTitles();
+    }
+}
