d65991fd0dc819c2da23ee7845730d7f305cd163
AndrewPonyk <andrew9999@ukr.net>
2017-07-10 11:49:59 +0300

    Logic/memory completed

--- Logic_Memory/scripts/utils.js
+++ Logic_Memory/scripts/utils.js
@@ -11,3 +11,18 @@ String.prototype.format = function () {
 random = function (max) {
     return Math.floor(Math.random(100) * max)
 }
+
+randomArray = function (max, length) {
+    var result = [];
+    for (var i = 0; i < length; i++) {
+        result.push(random(max));
+    }
+
+    return result;
+}
+
+sumArray = function (arr) {
+    return arr.reduce(function (a, b) {
+        return a + b;
+    }, 0)
+}
