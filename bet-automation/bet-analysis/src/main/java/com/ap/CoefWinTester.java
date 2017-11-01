8f96d164cb9d217c2d59bddf3213967b44626cef
ponykand <Andrii.Ponyk@externals.adidas.com>
2017-11-01 17:36:47 +0200

    random bets, and calculation

--- bet-automation/bet-analysis/src/main/java/com/ap/CoefWinTester.java
+++ bet-automation/bet-analysis/src/main/java/com/ap/CoefWinTester.java
@@ -1,11 +1,15 @@
 package com.ap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.math3.util.MathUtils;
 import org.apache.commons.math3.util.Precision;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CoefWinTester {
     public static class Constants{
@@ -13,39 +17,41 @@ public class CoefWinTester {
         public static Double WIN_COEF_FLAG;
     }
     public static void main(String[] args) {
-        Double[] coefs = new Double[]{1.12, 1.12, 1.12, 1.12, 1.12, 1.12, 1.12, 1.12, 1.11, 1.11, 1.11, 1.11, 1.11};
+        Double[] coefs = generateDoubleArray(39);
+        simulateWin(coefs, 3);
+        System.out.println(Arrays.toString(coefs));
+    }
+
+    private static void simulateWin(Double[] coefs, int loseCount) {
         List<Double> winsSumList = new ArrayList<>();
-        Constants.WIN_COEF_FLAG = 0.3;
+        Constants.WIN_COEF_FLAG = 0.21;
 
-        //while (Constants.WIN_COEF_FLAG <2){
-
-            for(int j = 0;j<coefs.length;j++){
-                Double currentSum = 11.0;
-                List<Integer> loseIndexes = Arrays.asList(j);
-                for (int i =0;i<coefs.length;i++) {
-                    Double betSum = getBetSum(currentSum);
-                    Double winSum = coefs[i] * betSum - betSum;
-                    //debug System.out.println(currentSum+"<-current: "+ i +" : betsum->" + betSum);
-                    if(!loseIndexes.contains(i)){
-                        currentSum = currentSum + winSum;
-                    } else {
-                        currentSum = currentSum - betSum;
-                    }
-
-                }
-                System.out.println("Loose index " + j +" and Result is: " + currentSum);
-                winsSumList.add(currentSum);
+        for(int j = 0;j<coefs.length;j++){
+            Double currentSum = 11.0;
+            List<Integer> loseIndexes = new ArrayList<>();
+            for (int i =0; i < loseCount; i++){
+                loseIndexes.add(ThreadLocalRandom.current().nextInt(coefs.length));
             }
+            for (int i =0;i<coefs.length;i++) {
+                Double betSum = getBetSum(currentSum);
+                Double winSum = coefs[i] * betSum - betSum;
+                //debug System.out.println(currentSum+"<-current: "+ i +" : betsum->" + betSum);
+                if(!loseIndexes.contains(i)){
+                    currentSum = currentSum + winSum;
+                } else {
+                    currentSum = currentSum - betSum;
+                }
 
-            System.out.println("For flag=" + Constants.WIN_COEF_FLAG +" Average win is : " +
-                    Precision.round(winsSumList.stream().mapToDouble(e->e).average().getAsDouble(),2));
-            //Constants.WIN_COEF_FLAG = Constants.WIN_COEF_FLAG + 0.05;
-        //}
+            }
+            System.out.println("Loose indexes are " + StringUtils.join(loseIndexes) +" and Result is: " + currentSum);
+            winsSumList.add(currentSum);
+        }
 
+        System.out.println("For flag=" + Constants.WIN_COEF_FLAG +" Average win is : " +
+                Precision.round(winsSumList.stream().mapToDouble(e->e).average().getAsDouble(),2));
     }
 
     public static Double getBetSum(Double currBalance){
-
         //old way
         //Double betSum = Constants.BET_BASE + currBalance%Constants.BET_BASE;
         Double betSum = 0d;
@@ -59,5 +65,25 @@ public class CoefWinTester {
         }
 
         return betSum;
+    }
+
+
+    public static Double[] generateDoubleArray(int size){
+        Double[] result = new Double[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = Precision.round(Math.random()/5.8, 2)+1;
+            result[i] = 1.11;
+            if(result[i] <1.06 ){
+                result[i] = Precision.round(Math.random()/6, 2)+1;
+                if(result[i] <1.06 ){
+                    result[i] = Precision.round(Math.random()/6, 2)+1;
+                    if(result[i] <1.05 ){
+                        result[i] = Precision.round(Math.random()/6, 2)+1;
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
