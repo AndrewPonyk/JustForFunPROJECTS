ef0318add86bc53dd7033e0cd0cea7cb5377b48d
AndrewPonyk <andrew9999@ukr.net>
2018-02-10 22:43:13 +0200

    fix tiebreaks

--- bet-automation/bet-analysis/src/main/java/com/ap/wincheckers/TennisWinChecker.java
+++ bet-automation/bet-analysis/src/main/java/com/ap/wincheckers/TennisWinChecker.java
@@ -1,6 +1,10 @@
 package com.ap.wincheckers;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TennisWinChecker implements WinChecker {
     /**
      * @param result, example '1-1(6-4,5-7,5-6) 0:30'
@@ -25,6 +29,26 @@ public class TennisWinChecker implements WinChecker {
             String[] lastSetStrings = setStrings[setStrings.length - 1].split("-");
             int[] lastSetResult = {Integer.valueOf(lastSetStrings[0]), Integer.valueOf(lastSetStrings[1])};
 
+            int advantagesOnTieBreak = -1;
+            if(result.contains(":")){
+                List<Integer> tieBreakResult = Arrays.stream(result
+                        .substring(result.indexOf(")")+1)
+                        .trim()
+                        .split(":"))
+                        .map(e->Integer.valueOf(e))
+                        .collect(Collectors.toList());
+                if(tieBreakResult.get(0) > tieBreakResult.get(1) && tieBreakResult.get(0) >=5){
+                    advantagesOnTieBreak = 1;
+                } else if(tieBreakResult.get(0) < tieBreakResult.get(1) && tieBreakResult.get(1) >=5){
+                    advantagesOnTieBreak = 2;
+                }
+
+                if(advantagesOnTieBreak > 0 && score[advantagesOnTieBreak-1] > 0 ){
+                    return advantagesOnTieBreak;
+                }
+
+            }
+
             if (lastSetResult[0] > lastSetResult[1] && lastSetResult[0] >= 5 && score[0] >= score[1]) {
                 return 1;
             } else if (lastSetResult[0] < lastSetResult[1] && lastSetResult[1] >= 5 && score[0] <= score[1]) {
@@ -38,7 +62,7 @@ public class TennisWinChecker implements WinChecker {
     }
 
     public static void main(String[] args) {
-        System.out.println(new TennisWinChecker().getWinner("1-1(6-4,5-7,7-6) 0:30"));
+        System.out.println(new TennisWinChecker().getWinner("1-1(6-2,2-6,4-5) 15:40"));
 
     }
 }
