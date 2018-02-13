8143d8e6fd09f1b0c5a1b41d2fd56bae08c287f0
ponykand <Andrii.Ponyk@externals.adidas.com>
2018-02-13 11:42:44 +0200

    added colored text in comeback utils

--- bet-automation/bet-analysis/src/main/java/com/ap/_5Comeback_Analyzer_Utils.java
+++ bet-automation/bet-analysis/src/main/java/com/ap/_5Comeback_Analyzer_Utils.java
@@ -4,7 +4,16 @@ package com.ap;
  * Created by andrii on 27.01.18.
  */
 public class _5Comeback_Analyzer_Utils {
+    // Color reset
+    public static final String RESET = "\033[0m";  // Text Reset
+    // Regular Colors
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+
+
     public static void main(String[] args) {
+        System.out.println(GREEN + "Colored text" +
+                RESET + " NORMAL");
         favoriteCross14CoefAndTheirFinalResult();
     }
 
