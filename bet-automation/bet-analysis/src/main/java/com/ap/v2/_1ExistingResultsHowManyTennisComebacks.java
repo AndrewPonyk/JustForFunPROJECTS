da2ed76fb0cd016211aa39d7207dc85627b22a77
AndrewPonyk <andrew9999@ukr.net>
2018-04-24 11:10:29 +0300

    v2 to analysis stubs

---
+++ bet-automation/bet-analysis/src/main/java/com/ap/v2/_1ExistingResultsHowManyTennisComebacks.java
@@ -0,0 +1,10 @@
+package com.ap.v2;
+
+import org.openqa.selenium.WebDriver;
+
+public class _1ExistingResultsHowManyTennisComebacks {
+    WebDriver driver;
+    public static void main(String[] args) {
+        System.out.println("show proportion of comebacks lose/win by day");
+    }
+}
---
+++ bet-automation/bet-analysis/src/main/java/com/ap/v2/_2CountOfComebacksWinLoseByDays.java
@@ -0,0 +1,7 @@
+package com.ap.v2;
+
+public class _2CountOfComebacksWinLoseByDays {
+    public static void main(String[] args) {
+
+    }
+}
