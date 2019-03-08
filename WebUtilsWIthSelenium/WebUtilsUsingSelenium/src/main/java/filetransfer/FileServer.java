18fb63d0bce868f3f3867cf55aa4b9c56665ec87
AndrewPonyk <andrii.ponyk@externals.adidas.com>
2019-03-08 23:27:47 +0200

    file server and client

---
+++ WebUtilsWIthSelenium/WebUtilsUsingSelenium/src/main/java/filetransfer/FileServer.java
@@ -0,0 +1,56 @@
+package filetransfer;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileServer extends Thread {
+
+    private ServerSocket ss;
+
+    public FileServer(int port) {
+        try {
+            ss = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                Socket clientSock = ss.accept();
+                saveFile(clientSock);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveFile(Socket clientSock) throws IOException {
+        DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+        FileOutputStream fos = new FileOutputStream("/home/pihura_olia/test.txt");
+        byte[] buffer = new byte[4096];
+
+        int filesize = 184; // Send file size in separate msg
+        int read = 0;
+        int totalRead = 0;
+        int remaining = filesize;
+        while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+            totalRead += read;
+            remaining -= read;
+            System.out.println("read " + totalRead + " bytes.");
+            fos.write(buffer, 0, read);
+        }
+
+        fos.close();
+        dis.close();
+    }
+
+    public static void main(String[] args) {
+        FileServer fs = new FileServer(4444);
+        fs.start();
+    }
+
+}
